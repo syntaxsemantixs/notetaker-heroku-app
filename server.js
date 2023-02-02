17acf8c4289e78d1098bac70d499c57ba0639464
syntaxsemantixs <gormeno2030@gmail.com>
2023-02-02 18:14:19 -0500

    first attempt at project

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 //import dependencies
 
 const app = express();
-const port = process.env.port || 3001
+const PORT = process.env.PORT || 3001
 
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
@@ -22,13 +22,20 @@ app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "public/notes.html"));
 });
 
-app.get("*", function(req, res){
+app.get("/api/notes/", function (req, res) {
+    fs.readFile(__dirname + "/db/db.json", (err, data) => {
+        var json = JSON.parse(data);
+        return res.json(json);
+    })
+});
+
+app.get("*", function (req, res) {
     res.sendFile((__dirname + "/public/index.html"));
 });
 
 //POST
 
-app.post("/api/notes", function (req, res){
+app.post("/api/notes", function (req, res) {
     addNote = req.body;
 
     fs.readFile(__dirname + "/db/db.json", (err, data) => {
@@ -41,12 +48,12 @@ app.post("/api/notes", function (req, res){
 
 //DELETE
 
-app.delete("/api/notes/:id", function (req, res){
+app.delete("/api/notes/:id", function (req, res) {
     let response = req.params;
     let id = response.id;
     console.log(`Note id: ${id} marked to be deleted`)
 
-    fs.readFile(__dirname + "/db/db.json", (err,data) => {
+    fs.readFile(__dirname + "/db/db.json", (err, data) => {
         const jsonFile = JSON.parse(data);
 
         const filteredJson = json.filter((element) => element.id !== id);
@@ -56,8 +63,8 @@ app.delete("/api/notes/:id", function (req, res){
     });
 });
 
-app.listen(process.env.PORT || 3001, () => {
-    console.log()
+app.listen(PORT, () => {
+    console.log(`Express server listening on http://localhost:${PORT}`)
 
     //finish console log message tomorrow. Check loccally then try heroku
 })
